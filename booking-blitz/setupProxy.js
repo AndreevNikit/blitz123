ef49039ad1c2ca98f491c125ac17906b7533e549
Animila <khristoforov-i@mail.ru>
2024-06-23 12:29:30 +0900

    преобразовал в dockerfile

--- booking-blitz/setupProxy.js
+++ booking-blitz/setupProxy.js
@@ -3,15 +3,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:5000/',
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'http://localhost:5000/',
+//       changeOrigin: true,
+//     })
+//   );
+// };
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
