e1f168ee368556ffceff2c9c927ad244dbcd0881
Animila <khristoforov-i@mail.ru>
2024-06-24 10:31:29 +0900

    fix

--- booking-blitz/setupProxy.js
+++ booking-blitz/setupProxy.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const app = express();
-
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'https://blitzykt.ru',
-//       changeOrigin: true,
-//     })
-//   );
-// };
-
-app.listen(3440, () => {
-  console.log('Server is running on port 5000');
-});
+// const express = require('express');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+//
+// const app = express();
+//
+// // module.exports = function(app) {
+// //   app.use(
+// //     '/api',
+// //     createProxyMiddleware({
+// //       target: 'https://blitzykt.ru',
+// //       changeOrigin: true,
+// //     })
+// //   );
+// // };
+//
+// app.listen(3440, () => {
+//   console.log('Server is running on port 5000');
+// });
