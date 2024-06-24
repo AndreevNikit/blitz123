1cc483f0e3ec1d343c73ee90c00ae2b95da2184a
Animila <khristoforov-i@mail.ru>
2024-06-24 11:06:31 +0900

    nginx

--- booking-blitz/setupProxy.js
+++ booking-blitz/setupProxy.js
@@ -13,6 +13,6 @@
 // //   );
 // // };
 //
-// app.listen(3440, () => {
-//   console.log('Server is running on port 5000');
-// });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
